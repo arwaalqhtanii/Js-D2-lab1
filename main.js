aad61a0d78db701ef2843657c1eb6c67dfa19fd1
arwaalqhtanii <alqhtaniarwa414@gmail.com>
2024-09-09 11:05:17 +0300

    write code js

---
+++ main.js
@@ -0,0 +1,37 @@
+
+//q1
+
+for (let i = 25; i >= 0; i--) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+//q2
+
+for (let i = 1; i <= 10; i++) {
+    let square = i * i;
+    console.log(`squqre ${i} is ${square}`);
+}
+
+//q3
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
+//q4
+
+let fibonacci = [0, 1];
+
+for (let i = 2; i < 10; i++) {
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+console.log( 'fibonacci', fibonacci);
+
+//q5
+for (let i = 1; i <= 3; i++) {
+    console.log(i);
+    for (let j = 1; j <= 10; j++) {
+        console.log(`i * j = ${i * j}`);
+    }
+}
